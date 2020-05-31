a02337a1d92892359b36879717e56ab8adaa0d74
Apoorva Srivastava <55443541+apoorvasrivastava98@users.noreply.github.com>
2020-05-31 18:52:24 +0530

    Create C++_basicdatatypes.cpp

---
+++ C++_basicdatatypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int p;
+    long q;
+    char r;
+    float s;
+    double t;
+   scanf("%d %ld %c %f %lf",&p,&q,&r,&s,&t);
+   printf("%d \n",p);
+    printf("%ld \n",q);
+    printf("%c \n",r);
+    printf("%f \n",s);
+    printf("%lf",t);
+    return 0;
+}
