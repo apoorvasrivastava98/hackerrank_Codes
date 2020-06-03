58a18f120cd8bcaeeaea7047feb422fe15fbd2d0
Apoorva Srivastava <55443541+apoorvasrivastava98@users.noreply.github.com>
2020-06-03 23:32:40 +0530

    Create 30Days_datatypes.py

---
+++ 30Days_datatypes.py
@@ -0,0 +1,19 @@
+  int integer;
+    double doub;
+    
+    // Declare second integer, double, and String variables.
+    
+    // Read and save an integer, double, and String to your variables.
+    scanf("%d %lf",&integer,&doub);
+    char str[100];
+  scanf("%*[\n]%[^\n]",str);
+    // Print the sum of both integer variables on a new line.
+    integer =i+integer;
+    printf("%d \n",integer);
+    // Print the sum of the double variables on a new line.
+      doub =d+doub;
+    printf("%0.1lf \n",doub);
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+ 
+    printf("%s%s",s,str);
